e62cdd45b35dbc3c51eb7ea35a328f3a3af153e5
Yuki Nishijima <yuki24@hey.com>
2021-08-15 14:44:48 -0400

    Set up test

---
+++ test/helpers/form_helper_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ViewHelperTest < ActionView::TestCase
+  test "foo" do
+    assert false
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "shoelace/rails"
 
 require "minitest/autorun"
+
+require "action_controller"
+require "action_view"
+require "action_view/testing/resolvers"
