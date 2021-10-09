537d8ec2ad11fb6edffef659074f2cfc9b939b81
Yuki Nishijima <yuki24@hey.com>
2021-10-08 21:54:27 -0400

    Reduce files in the dummy app

--- test/dummy_app/config/webpack/test.js
+++ test/dummy_app/config/webpack/test.js
@@ -1,5 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-const webpackConfig = require('./base')
+const { webpackConfig } = require('@rails/webpacker')
 
 module.exports = webpackConfig
