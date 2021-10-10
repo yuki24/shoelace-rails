1eeaa50ae86da13ad39910366b44fa669190981b
Yuki Nishijima <yuki24@hey.com>
2021-10-10 16:07:06 -0400

    Fix Safari compatibility

--- src/turbo/index.ts
+++ src/turbo/index.ts
@@ -1,7 +1,6 @@
-import { polyfillFormDataEvent } from "./polyfills/formdata_event"
+import "./polyfills/formdata-event"
 import { SlTurboFormElement } from "./sl-turbo-form"
 
 export const startTurbo = () => {
-  polyfillFormDataEvent(window)
   customElements.define("sl-turbo-form", SlTurboFormElement)
 }
--- src/turbo/sl-turbo-form.ts
+++ src/turbo/sl-turbo-form.ts
@@ -2,6 +2,10 @@ interface SlForm extends HTMLElement {
   getFormData: () => FormData
 }
 
+interface SlButton extends HTMLElement {
+  submit: boolean
+}
+
 interface FormDataEvent extends Event {
   formData: FormData
 }
@@ -16,15 +20,30 @@ const SHADOW_DOM_TEMPLATE = `
   </sl-form>
 `
 
-const isOnSafari = () => /Apple Computer/.test(navigator.vendor)
+const submittersByForm: WeakMap<HTMLFormElement, HTMLElement> = new WeakMap()
 
 const cloneAttributes = (target, source) =>
   [...source.attributes]
-    .filter(({ nodeName }) => !(['id', 'class'].includes(nodeName)))
+    .filter(({ nodeName }) => !["id", "class"].includes(nodeName))
     .forEach(({ nodeName, nodeValue }) => target.setAttribute(nodeName, nodeValue))
+
+const isOnSafari = () => /Apple Computer/.test(navigator.vendor)
+
+const findSubmitterFromClickTarget = (target: EventTarget | null): HTMLElement => {
+  const element = target instanceof Element ? target : target instanceof Node ? target.parentElement : null
+
+  if (element?.tagName === "SL-BUTTON") {
+    const slButton = element as SlButton
+    return slButton.submit ? slButton : null
+  } else {
+    const candidate = element?.closest("input, button") as HTMLInputElement | HTMLFormElement
+    return candidate?.type === "submit" ? candidate : null
+  }
+}
 
 export class SlTurboFormElement extends HTMLElement {
   private readonly form: HTMLFormElement
+  private called: boolean
 
   static get observedAttributes() {
     return ["action", "method", "enctype", "accept-charset", "data"]
@@ -47,11 +66,19 @@ export class SlTurboFormElement extends HTMLElement {
   }
 
   connectedCallback() {
+    if (isOnSafari()) {
+      this.addEventListener("click", this.clickCaptured, true)
+    }
+
     this.addEventListener("sl-submit", this.handleSubmit)
     this.addEventListener("formdata", this.handleFormData)
   }
 
   disconnectedCallback() {
+    if (isOnSafari()) {
+      this.removeEventListener("click", this.clickCaptured, true)
+    }
+
     this.removeEventListener("sl-submit", this.handleSubmit)
     this.removeEventListener("formdata", this.handleFormData)
   }
@@ -59,7 +86,8 @@ export class SlTurboFormElement extends HTMLElement {
   handleFormData = (event: FormDataEvent) => {
     const { formData, target } = event
 
-    if (this.form === target) {
+    if (this.form === target && !this.called) {
+      this.called = true
       const slForm = this.shadowRoot.querySelector("sl-form") as SlForm
 
       for (const [key, value] of slForm.getFormData().entries()) {
@@ -70,21 +98,26 @@ export class SlTurboFormElement extends HTMLElement {
 
   handleSubmit = (event: CustomEvent) => {
     event.stopImmediatePropagation()
-    const submitter = document.activeElement
     const submitEvent = new CustomEvent("submit", { bubbles: true, cancelable: true }) as SubmitEvent
+
     if (isOnSafari()) {
+      const submitter = submittersByForm.get(this.form)
       Object.defineProperty(submitEvent, "submitter", { get: () => submitter })
     } else {
-      submitEvent.submitter = submitter
+      submitEvent.submitter = document.activeElement
     }
 
     const cancelled = this.form.dispatchEvent(submitEvent)
     if (cancelled) {
-      if (isOnSafari()) {
-        new FormData(this.form)
-      }
-
       this.form.submit()
+    }
+  }
+
+  clickCaptured = (event: Event) => {
+    const submitter = findSubmitterFromClickTarget(event.target)
+
+    if (submitter) {
+      submittersByForm.set(this.form, submitter)
     }
   }
 }
