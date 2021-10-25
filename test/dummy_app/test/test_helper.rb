1dd761ba6dfabd1b8ea6024efc6d29e1737bf0b0
Yuki Nishijima <yuki24@hey.com>
2021-10-24 22:12:22 -0400

    Use the capybara-shadowdom gem

--- test/dummy_app/test/test_helper.rb
+++ test/dummy_app/test/test_helper.rb
@@ -10,38 +10,6 @@ end
 
 Capybara.server = :webrick
 
-class Capybara::Node::ShadowRoot < Capybara::Node::Element
-  def host
-    evaluate_script("this.host")
-  end
-
-  def text(type = nil, normalize_ws: false)
-    all("*")
-      .map {|node| node.text(type, normalize_ws: normalize_ws) }
-      .join
-  end
-end
-
-module ShadowRootSupport
-  # Adds a way to retrieve the shadow root object of an element.
-  #
-  # @example
-  #
-  #   within all("sl-input")[0].shadow_root do
-  #     puts text
-  #     assert_text "Email"
-  #     fill_in "user[email]", with: "ynishijima@pivotal.io"
-  #   end
-  #
-  def shadow_root
-    root_node = evaluate_script("this.shadowRoot")
-
-    Capybara::Node::ShadowRoot.new(session, root_node.base, nil, nil) if root_node
-  end
-end
-
-Capybara::Node::Element.include ShadowRootSupport
-
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Shoelace::Testing
 
