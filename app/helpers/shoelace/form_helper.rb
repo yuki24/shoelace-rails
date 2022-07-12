bb981ed05825707cef89d70a7d1699c12cd0ba9b
Yuki Nishijima <yuki24@hey.com>
2022-07-12 21:51:42 +0900

    Fixes a bug where the @object needs to respond to #errors

--- app/helpers/shoelace/form_helper.rb
+++ app/helpers/shoelace/form_helper.rb
@@ -21,7 +21,7 @@ module Shoelace
 
         options["size"] = options["maxlength"] unless options.key?("size")
         options["type"] ||= field_type
-        options["class"] ||= [options["class"], Shoelace::FormHelper.invalid_input_class_name].compact.join(" ") if @object.errors[@method_name].present?
+        options["class"] ||= [options["class"], Shoelace::FormHelper.invalid_input_class_name].compact.join(" ") if @object.respond_to?(:errors) && @object.errors[@method_name].present?
 
         add_default_name_and_id(options)
 
