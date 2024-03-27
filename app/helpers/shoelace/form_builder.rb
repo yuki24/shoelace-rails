b207a8e6bb80c9d5245bdd50315eb18010c06ad0
Yuki Nishijima <yuki@xpillars.software>
2024-03-27 16:14:41 +0900

    Address deprecation warning

--- app/helpers/shoelace/form_builder.rb
+++ app/helpers/shoelace/form_builder.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
-require_relative './components/sl_checkbox'
-require_relative './components/sl_color_picker'
-require_relative './components/sl_input'
-require_relative './components/sl_radio_group'
-require_relative './components/sl_range'
-require_relative './components/sl_select'
-require_relative './components/sl_switch'
-require_relative './components/sl_textarea'
+require_relative './sl_form_builder'
 
 module Shoelace
   FormBuilder = SlFormBuilder
--- app/helpers/shoelace/form_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require_relative './sl_form_builder'
-
-module Shoelace
-  FormHelper = SlFormHelper
-  deprecate_constant :FormHelper
-end
